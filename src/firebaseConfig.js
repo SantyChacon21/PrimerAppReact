5bc96b1098178cf1140067e8b548c8cb4af44629
SantyChacon21 <santychacon21@gmail.com>
2023-01-19 18:06:48 -0300

    add readme

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,13 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBU9SNcSOw_vsCo0Vu_L6HMdrzqSvlpqJY",
-  authDomain: "superminicars-23.firebaseapp.com",
-  projectId: "superminicars-23",
-  storageBucket: "superminicars-23.appspot.com",
-  messagingSenderId: "797366333819",
-  appId: "1:797366333819:web:2434e59be5342c2e054dde",
-  measurementId: "G-PN0BJ6BTK1"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
